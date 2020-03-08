e8bb257323fbc45349dea572cbdfd3dac8118595
PabloMuralles <pablomuralles28@gmail.com>
2020-03-08 17:45:57 -0600

    intento descompresion

---
+++ Lab4_Compresion/Descomprecion/Descompresion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab4_Compresion.Descomprecion
+{
+    public class Descompresion
+    {
+        string NombreArchivo = string.Empty;
+
+        const int longitud = 1000000;
+        public Descompresion(string nombrearchivocontrolller, string leercontroller)
+        {
+            NombreArchivo = nombrearchivocontrolller;
+        }
+        public void Descomprimir(string leer)
+        {
+
+            using (var stream = new FileStream(leer, FileMode.Open))
+            {
+                using (var reader = new BinaryReader(stream))
+                {
+                    var buffer = new byte[longitud];
+                    buffer = reader.ReadBytes(longitud);
+                    foreach (byte bit in buffer)
+                    {
+                    
+                    }
+                }
+            }
+ 
+        }
+    }
+}
